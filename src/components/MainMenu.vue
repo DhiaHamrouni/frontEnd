dce2b7fff95f96f93a7a8feaf3e776ba09a140db
DhiaHamrouni <dhia.hamrouni@esprit.tn>
2023-12-21 13:59:44 +0100

    first commit

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-app id="inspire">
+        <v-main>
+            <v-container
+                class="py-8 px-6"
+                fluid
+            >
+                <v-row >
+                    <v-col
+                        v-for="card in cards"
+                        :key="card"
+                        cols="12"
+                        class="bg-black text-center"
+                    >
+                        <v-card >
+
+                            <v-list lines="two">
+                                <v-list-subheader>{{ card }}</v-list-subheader>
+
+                                <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-avatar color="red">
+                                            <v-icon dark>
+                                                mdi-alert-circle
+                                            </v-icon>
+                                        </v-avatar>
+                                    </template>
+
+                                    <v-list-item-content>
+                                        <v-list-item-title class="text-left"> Timmi Abscence </v-list-item-title>
+                                        <v-list-item-subtitle class="text-left">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                                        </v-list-item-subtitle>
+                                    </v-list-item-content>
+
+
+                                </v-list-item>
+                            <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-avatar color="red">
+                                            <v-icon dark>
+                                                mdi-alert-circle
+                                            </v-icon>
+                                        </v-avatar>
+                                    </template>
+
+                                    <v-list-item-title class="text-left"> Timmi Abscence </v-list-item-title>
+                                    <v-list-item-subtitle class="text-left">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                                    </v-list-item-subtitle>
+                                </v-list-item>
+                                <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-avatar color="gold">
+                                            <v-icon dark>
+                                                mdi-alert-circle
+                                            </v-icon>
+                                        </v-avatar>
+                                    </template>
+
+                                    <v-list-item-content>
+                                        <v-list-item-title class="text-left"> Poplee Entretien </v-list-item-title>
+                                        <v-list-item-subtitle class="text-left">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                                        </v-list-item-subtitle>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script setup>
+
+const cards = ['All Notifications']
+
+
+</script>
+
+<script>
+export default {
+    data: () => ({
+        cards: ['All Notifications', 'Yesterday'],
+        drawer: null,
+    }),
+}
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-navigation-drawer v-model="drawer" expand-on-hover rail>
+        <v-sheet class="bg-yellow-darken-2">
+            <v-avatar class="mb-4" color="grey darken-1" size="64">
+                <v-img src="/src/assets/logo.png" contain></v-img>
+            </v-avatar>
+
+            <div style="font-family: 'Tahoma'">John.Doe@email.com</div>
+        </v-sheet>
+
+        <v-divider></v-divider>
+
+        <v-list>
+            <v-list-item v-for="([icon, text, number], index) in links" :key="index" link>
+                <template v-slot:prepend>
+                    <v-icon>{{ icon }}</v-icon>
+                </template>
+
+                <v-list-item-title>{{ text }}</v-list-item-title>
+
+                <template v-slot:append>
+                    <v-badge color="grey" overlap>
+                        <template v-slot:badge>{{ number }}</template>
+                    </v-badge>
+                </template>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const links = [
+    ['mdi-email', 'Applications',3],
+    ['mdi-account', 'Poplee Entretiens',1],
+    ['mdi-currency-usd', 'Cleemy Notes de frais', 0],
+    ['mdi-account-alert', 'Timmi Abscences', 2],
+]
+const drawer = ref(null)
+</script>
